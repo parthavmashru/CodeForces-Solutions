0417daa143dcc253f4cbaccc37c2803ebb271a0d
parthavmashru <mashruparthav1@gmail.com>
2026-09-20 23:45:37 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        long long n, m, i, j;
+        cin >> n >> m >> i >> j;
+        
+        cout << 1 << " " << 1 << " " << n << " " << m << "
+";
+    }
+    
+    return 0;
+}
