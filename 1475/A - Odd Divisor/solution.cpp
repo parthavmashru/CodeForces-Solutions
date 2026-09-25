7f8a307cd307857a10c43c13b87bc62e5c0b8951
parthavmashru <mashruparthav1@gmail.com>
2026-09-25 23:34:37 +0530

    Add Odd Divisor [A] from Codeforces

---
+++ 1475/A - Odd Divisor/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int t;cin >> t;
+    while(t--)
+    {
+        ll n;cin >> n;
+        if((n&(n-1))==0)cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+}
